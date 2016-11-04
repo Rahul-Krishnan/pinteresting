169c0fe4cf5b1917c5c879aeb1bf09d872ec30e9
Rahul Krishnan <rk2211@gmail.com>
2016-11-03 21:13:17 -0400

    changed validation

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,5 +2,7 @@ class Pin < ApplicationRecord
   belongs_to :user
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+  validates_attachment :image, presence: true,
+  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
+  size: { in: 0..10000.kilobytes }
 end
