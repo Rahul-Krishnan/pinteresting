98e327ecd5971716c5367ecdba7a8e745324b54c
Rahul Krishnan <rk2211@gmail.com>
2016-11-06 19:52:59 -0500

    increased pin count per page

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pins = Pin.all.order(created_at: :desc).paginate(:page => params[:page], :per_page => 10)
+    @pins = Pin.all.order(created_at: :desc).paginate(:page => params[:page], :per_page => 12)
   end
 
   def show
